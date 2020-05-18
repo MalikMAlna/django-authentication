9586eca4899955a0ffeea27a9903955202c3a5d7
Mike A <jimalik2525@gmail.com>
2020-05-18 16:36:22 -0400

    Added AccountAdmin for the UserAdmin to more easily access items in the Admin Panel

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
 from .models import Account
+from django.contrib.auth.admin import UserAdmin
 
-admin.site.register(Account)
+# https://www.youtube.com/
+# watch?v=XJU9vRARkGo&list=PLgCYzUzKIBE_dil025VAJnDjNZHHHR9mW&index=13
+
+
+class AccountAdmin(UserAdmin):
+    list_display = ('email', 'username', 'display_name', 'date_joined',
+                    'last_login', 'is_admin', 'is_staff')
+    search_fields = ('email', 'username', 'display_name', 'age')
+    readonly_fields = ('date_joined', 'last_login')
+
+    filter_horizontal = ()
+    list_filter = ()
+    fieldsets = ()
+
+
+admin.site.register(Account, AccountAdmin)
