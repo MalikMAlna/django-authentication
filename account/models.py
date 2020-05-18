1c2410e7a95ca848679bdfb486334eb1cdbba7ae
Mike A <jimalik2525@gmail.com>
2020-05-18 15:53:03 -0400

    Added Account as AbstractBaseUser, now adding the account manager

--- account/models.py
+++ account/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
-# Create your models here.
+# https://www.youtube.com/watch?v=eCeRC7E8Z7Y
+
+
+class AccountManager(BaseUserManager):
+    pass
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(verbose_name='email', max_length=60, unique=True)
+    username = models.CharField(max_length=25, unique=True)
+    display_name = models.CharField(max_length=25, unique=True)
+    homepage = models.URLField(null=True, blank=True)
+    age = models.IntegerField(null=True, blank=True)
+    date_joined = models.DateTimeField(
+        verbose_name='date-joined', auto_now_add=True)
+    last_login = models.DateTimeField(verbose_name='last-login', auto_now=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['username', 'age', 'display_name']
+
+    def __str__(self):
+        return self.username
+
+    def has_perm(self, perm, obj=None):
+        return self.is_admin
+
+    def has_module_perms(self, app_label):
+        return True
--- authentication/settings.py
+++ authentication/settings.py
@@ -33,13 +33,13 @@ LOGOUT_URL = "/"
 # Application definition
 
 INSTALLED_APPS = [
-    'account.apps.AccountConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'account.apps.AccountConfig',
 ]
 
 
@@ -126,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTH_USER_MODEL = account.Account
